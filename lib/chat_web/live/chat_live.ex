cb0ff193a18e39949094f3e7ea7e05dd568a9789
Steve Freeman <steve@indieit.com>
2023-04-16 14:10:47 -0500

    added temporary_assigns to history and used phx-update=append with id the message itself

--- lib/chat_web/live/chat_live.ex
+++ lib/chat_web/live/chat_live.ex
@@ -2,7 +2,8 @@ defmodule ChatWeb.ChatLive do
   use ChatWeb, :live_view
 
   def mount(%{"room_id" => room_id}, _session, socket) do
-    {:ok, assign(socket, room: room_id, prompt: "", response: "", history: [])}
+    {:ok, assign(socket, room: room_id, prompt: "", response: "", history: []),
+     temporary_assigns: [history: []]}
   end
 
   def handle_event("prompt", %{"prompt" => prompt}, socket) do
@@ -11,22 +12,20 @@ defmodule ChatWeb.ChatLive do
     response = Chat.OpenAI.send(prompt)
     IO.inspect(response, label: "RESPONSE")
 
-    new_history = [prompt, response | socket.assigns.history]
-    IO.inspect(new_history, label: "HISTORY")
+    history = [prompt, response]
+    IO.inspect(history, label: "HISTORY")
 
-    socket = assign(socket, prompt: prompt, response: response, history: new_history)
+    socket = assign(socket, prompt: prompt, response: response, history: history)
     IO.inspect(socket, label: "SOCKET")
 
     {:noreply, socket}
   end
 
-  # NOTE : make "question" and "response" into users...
-
   def render(assigns) do
     ~H"""
-    <pre :for={msg <- @history}><%= msg %></pre>
+    <div phx-update="append" id="msg">
+    <pre :for={msg <- @history} id={"msg-#{msg}"}><%= msg %></pre></div>
     <br />
-
     <form phx-submit="prompt">
       <input
         type="text"
@@ -36,6 +35,8 @@ defmodule ChatWeb.ChatLive do
         autocomplete="off"
       />
     </form>
+    <br>
+    <h1>You are chatting with GPT in the <em><%= @room %></em> room</h1>
     """
   end
 end
