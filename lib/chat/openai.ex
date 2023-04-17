45daf5950a972822afb08979e2665af7ea410a8c
Steve Freeman <steve@indieit.com>
2023-04-17 14:59:33 -0500

    added gpt4 option in genserver

--- lib/chat/openai.ex
+++ lib/chat/openai.ex
@@ -16,8 +16,11 @@ defmodule Chat.OpenAI do
 
   @impl true
   def handle_call({:msg, m}, _from, msgs) do
+    gpt3 = "gpt-3.5-turbo"
+    # gpt4 = "gpt-4"
+
     with msgs <- msgs ++ [new_msg(m)] do
-      case ExOpenAI.Chat.create_chat_completion(msgs, "gpt-3.5-turbo") do
+      case ExOpenAI.Chat.create_chat_completion(msgs, gpt3) do
         {:ok, res} ->
           first = List.first(res.choices)
           {:reply, first.message.content, msgs ++ [first.message]}
