f4b8d9bc2570bdf3573e5ec39d03f8957d81fc08
Steve Freeman <steve@indieit.com>
2023-04-26 11:10:39 -0500

    tweaked header

--- lib/chat_web/live/chat_live.ex
+++ lib/chat_web/live/chat_live.ex
@@ -38,7 +38,9 @@ defmodule ChatWeb.ChatLive do
   def render(assigns) do
     ~H"""
     <div phx-update="append" id="msg">
-    <md-block :for={prompt <- [@prompt]} id={UUID.uuid4()}><%= prompt %></md-block>
+    <md-block :for={prompt <- [@prompt]} id={UUID.uuid4()}><%= prompt %></md-block></div>
+    <div class="pt-6 pb-8">   </div>
+    <div phx-update="append" id="msg">
     <md-block :for={response <- [@response]} id={UUID.uuid4()}><%= response %></md-block></div>
     <br>
     <form phx-submit="prompt">
