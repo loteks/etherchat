ac968eb7d3be865f0ed5a01458d1cd2d1f6a0a3e
Steve Freeman <steve@indieit.com>
2023-05-02 12:34:11 -0500

    copied openai config to runtime.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,6 +48,15 @@ if config_env() == :prod do
     ],
     secret_key_base: secret_key_base
 
+  # ## OpenAI Support
+  config :ex_openai,
+    # find it at https://platform.openai.com/account/api-keys
+    api_key: System.get_env("OPENAI_API_KEY"),
+    # find it at https://platform.openai.com/account/api-keys
+    organization_key: System.get_env("OPENAI_ORGANIZATION_KEY"),
+    # optional, passed to [HTTPoison.Request](https://hexdocs.pm/httpoison/HTTPoison.Request.html) options
+    http_options: [recv_timeout: 50_000]
+
   # ## SSL Support
   #
   # To get SSL working, you will need to add the `https` key
